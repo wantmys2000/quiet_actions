522f676d531781ea2d3a4c77d6e463c398ee45a2
Steven Buxton <want_my_s2000@hotmail.com>
2014-06-04 23:13:18 -0400

    SB -- Add code

--- lib/quiet_actions.rb
+++ lib/quiet_actions.rb
@@ -1,5 +1,31 @@
 require "quiet_actions/version"
+require "logger"
 
 module QuietActions
-  # Your code goes here...
+  class Engine < ::Rails::Engine
+    config.quiet_actions = true
+    config.quiet_action_paths = []
+
+    initializer 'quiet_actions', :after => 'sprockets.environment' do |app|
+      next unless app.config.quiet_actions
+
+      KEY = 'quiet_actions.old_level'
+      QUIET_ACTIONS = app.config.quiet_action_paths
+
+      Rails::Rack::Logger.class_eval do
+        def call_with_quiet_actions(env)
+          begin
+            if QUIET_ACTIONS.include?(env['PATH_INFO'])
+              env[KEY] = Rails.logger.level
+              Rails.logger.level = Logger::ERROR
+            end
+            call_without_quiet_actions(env)
+          ensure
+            Rails.logger.level = env[KEY] if env[KEY]
+          end
+        end
+        alias_method_chain :call, :quiet_actions
+      end
+    end
+  end
 end
